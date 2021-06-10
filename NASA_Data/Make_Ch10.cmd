9e8dd7fb46153f2883a4986a7a7845178d0db9a1
bbaggerman <bob.git@baggerman.org>
2021-06-10 11:32:41 -0500

    Windows command file to make a single Ch 10 file from NASA Matlab data file

---
+++ NASA_Data/Make_Ch10.cmd
@@ -0,0 +1,19 @@
+@echo off
+
+REM Make_Ch10.cmd <Dataset Dir> <Data filename>
+REM where
+REM   <Dataset Dir>   - Name of the directory of a set of NASA data files. E.g. Tail_652_1
+REM   <Data filename> - Name of an individual NASA Matlab datafile. E.g. 652200101120916
+REM
+REM Example - Make_Ch10 Tail_652_1 652200101120916
+
+echo %1  %2
+
+REM Convert NASA Matlab file into CSV data file
+python make_nasa_pcm_data.py %1 %2.mat
+
+REM Convert CSV file into Ch 10 file
+..\Release\SynthCh10Gen -v -N CSV\%1\%2_pcm.csv Ch10\%1\%2.ch10
+
+
+
