1485e1fd77e60e28b0f9e046d4d2d0ee7e89d1da
Stephen Frees <stephenf93@gmail.com>
2025-08-20 09:18:23 -0500

    Removed parameter pointer in PushData method

--- SynthCh10Gen/Ch10Channel.h
+++ SynthCh10Gen/Ch10Channel.h
@@ -37,7 +37,7 @@ public:
   ~Ch10Channel();
 
   void Init(Ch10Writer* writer, Ch10Formatter* formatter, ChannelType type, std::string name, std::string sourcePrefix);
-  void PushData(ClSimState* simState, int64_t* clockTicks); // push data from sim into packet
+  void PushData(ClSimState* simState, int64_t clockTicks); // push data from sim into packet
   void CommitPacket(); // commit full packet to file stream
 
   std::string GetTMATS(ClTmatsIndexes index);
