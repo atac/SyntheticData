e013c2337ac4830f73970eeb6dd3cddf979ac478
Stephen Frees <stephenf93@gmail.com>
2025-09-10 14:21:53 -0500

    Added CLI support for SynthCh10Gen

--- SynthCh10Gen/Program.cpp
+++ SynthCh10Gen/Program.cpp
@@ -28,15 +28,60 @@ bool StatusOk(ControllerStatus val) {
 }
 
 
+void showHelpString() {
+  string helpstring = 
+    R"(usage: SynthCh10Gen [options] <config>
+
+  <config>:
+    Path to a configuration file that defines data sources and output format.
+    See "Configuration Specification.md" for details.
+
+  [options]:
+    --help          Show this help string
+)";
+
+  printf(helpstring.data());
+}
 
 
-int main(int iArgc, char* aszArgv[]) {
+struct CLIParams {
+  string configPathname;
+} cliParams;
+
+bool processCLI(int argCount, char* args[]) {
+  if (argCount != 2)
+    return false;
+
+  for (int i = 1; i < argCount; i++) {
+    string a = args[i];
+
+    if (a[0] == '-') { // argument is an option
+      if (a == "--help" || a == "-h")
+        return false; // show help string and exit
+    }
+    else
+    {
+      if (i == 1) { // expect configuration pathname
+        cliParams.configPathname = a;
+      }
+    }
+  }
+
+  return true;
+}
+
+
+int main(int iArgc, char* aszArgv[]) 
+{
+  if (!processCLI(iArgc, aszArgv)) {
+    showHelpString();
+    return 0;
+  }
+
   ControllerStatus result = ControllerStatus::OK;
 
   GenerationController* controller = new GenerationController();
-
-  //result = controller->Init("C:\\atac\\synthetic\\configs\\test_config_9.json");
-  result = controller->Init("C:\\data\\synthetic\\simulfour\\SynthGenConfig.json");
+  result = controller->Init(cliParams.configPathname);
   if (!StatusOk(result))
     return 1;
 
