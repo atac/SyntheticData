acb678583e734d13a2096be26a048c338d5d4566
Stephen Frees <stephenf93@gmail.com>
2026-03-17 16:56:08 -0500

    Fixed configuration parsing sequence causing mappings to be ignored

--- SynthCh10Gen/Configuration.cpp
+++ SynthCh10Gen/Configuration.cpp
@@ -33,8 +33,8 @@ void Config::ParseConfig() {
       return;
 
     ParseGeneralInfo();
-    ParseDataSources();
     ParseMappings();
+    ParseDataSources();
     ParseChannels();
   }
   catch (json::exception e) {
